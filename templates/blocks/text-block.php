51b04d0a2e74316d539cb50dd444df4e667e69f4
David <david@thisisparachute.com>
2020-08-05 11:01:19 +0100

    latest changes

---
+++ templates/blocks/text-block.php
@@ -0,0 +1,22 @@
+
+
+<div class="text-container">
+    <div class="container">
+        <?php if (get_field('title')) { ?>
+            <div class="row">
+                <div class="col-md-12">
+                    <header>
+                        <h2><?php echo get_field('title'); ?></h2>
+                    </header>
+                </div>
+            </div>
+        <?php }?>
+        <div class="row">
+            <div class="col-md-12">
+                <?php the_field('content');?>
+            </div>
+        </div>
+    </div>
+</div>
+
+ 
