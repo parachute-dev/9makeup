5eb1a6a1f12217b5d71074d33f88159c33a9a4e1
David <david@thisisparachute.com>
2020-08-07 11:37:07 +0100

    changes

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 
 	<meta name="viewport" content="width=device-width">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/base.css?v=1212asdfsdsdfsdfsdff2">
+	<link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/base.css?v=121asdfasdf2asdfsdsdfsdfsdff2">
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon.ico" />
 	<link rel="stylesheet" href="https://actors-temple.beta.parachute.digital/wp-content/themes/parachute-boilerplate-david/assets/js/lib/mmenu/mburger.css"> 
 
